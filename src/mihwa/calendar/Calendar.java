e80dfe90814925f3893d263fbed73c825929b303
Administrator <Administrator@DESKTOP-1QVE54M>
2018-05-18 12:42:15 +0900

    Update calendar main()

--- src/mihwa/calendar/Calendar.java
+++ src/mihwa/calendar/Calendar.java
@@ -2,8 +2,8 @@ package mihwa.calendar;
 
 public class Calendar {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
+	public static void main(String[] args) {//mainÇÔ¼ö
+		System.out.println("Hello, Calendar");//
 	}
 
 }
