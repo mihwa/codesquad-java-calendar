5185b9080b20ba491c42538b67ae266859c3e4ad
Administrator <Administrator@DESKTOP-1QVE54M>
2018-05-18 12:48:06 +0900

    Fix encoding problem

--- src/mihwa/calendar/Calendar.java
+++ src/mihwa/calendar/Calendar.java
@@ -2,9 +2,9 @@ package mihwa.calendar;
 
 public class Calendar {
 
-	public static void main(String[] args) {//mainÇÔ¼ö
-		System.out.println("ÀÏ ¿ù È­ ¼ö ¸ñ ±İ Åä");//
-		System.out.println("--------------");//
+	public static void main(String[] args) {//mainï¿½Ô¼ï¿½
+		System.out.println("ì¼   ì›”   í™”    ìˆ˜   ëª©   ê¸ˆ   í† ");//
+		System.out.println("--------------------");//
 		System.out.println(" 1 2  3  4  5  6  7");//
 		System.out.println(" 8 9 10 11 12 13 14");//
 		System.out.println("15 16 17 18 19 20 21");//
